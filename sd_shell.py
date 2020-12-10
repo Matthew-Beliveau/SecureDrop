9abc7f808778bfa9eadc9702c61a69522ad63bd9
Yorgo Kennos <yorgokennos@gmail.com>
2020-12-09 21:17:49 -0500

    Update sd_shell.py

--- sd_shell.py
+++ sd_shell.py
@@ -1,14 +1,65 @@
+#!/usr/bin/env python
 from cmd import Cmd
 from getpass import getpass
+from threading import Thread
+import time
 import os.path
 import json
 import re
 import crypt
 import secrets
+import sched
 
 MAX_TIMES_ALLOWED_TO_ENTER = 3
 
 USER_LIST = "./user_list.json"
+
+ONLINE_CONTACTS = {}
+
+
+class ScanTask:
+    def __init__(self):
+        self._running = True
+
+    def terminate(self):
+        self._running = False
+
+    def run(self):
+        self.scan_for_online_contacts()
+
+    s = sched.scheduler(time.time, time.sleep)
+
+    # Scheduled function to scan network on a certain socket to determine
+    # online contacts.
+    # TODO: add implementation in form of separate file.
+    def scan(self, sc):
+        """
+        This function is currently a placeholder for a real scanning
+        function/file. Real scan is to be added later. The plan is to have the
+        scan return a dictionary and store it in the variable ONLINE_CONTACTS.
+
+        Example code for how this function could interact with ONLINE_CONTACTS
+        is as follows:
+        global ONLINE_CONTACTS
+        ONLINE_CONTACTS = random.randint(100, size=(3))
+        """
+        if not self._running:
+            return None
+        self.s.enter(5, 1, self.scan, (sc,))
+
+    # Helper function to run scan on a certain interval. This function is ran
+    # on a separate thread.
+    def scan_for_online_contacts(self):
+        self.s.enter(5, 1, self.scan, (self.s,))
+        self.s.run()
+
+
+# Function to listen on a certain socket for other applications scanning,
+# to potentially communicate about online contacts and file transfer. This
+# function is ran on a separate thread.
+def listen_for_scan():
+    pass
+
 
 def password_check(password):
     """
@@ -54,21 +105,23 @@ def password_check(password):
         "symbol_error": symbol_error,
     }
 
+
 def get_password():
     password = getpass("Enter password: ")
     return password
 
+
 def check_email():
-    #function check_email allows the user to attempt to enter their password 3 times before exiting 
+    # function check_email allows the user to attempt to enter their password 3 times before exiting
 
     # regular expression for validating email
-    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    regex = "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
 
     email_enter_attempts = 0
 
     while email_enter_attempts < MAX_TIMES_ALLOWED_TO_ENTER:
         email = input("Enter email: ")
-        if(re.search(regex, email)):
+        if re.search(regex, email):
             print("Valid Email")
             email_enter_attempts = MAX_TIMES_ALLOWED_TO_ENTER
             return email
@@ -76,13 +129,16 @@ def check_email():
             print("Invalid Email")
             email_enter_attempts += 1
     return None
-    
+
+
 def register_email():
     email = input("Enter email: ")
     return email
 
+
 def hash_password(password, salt):
     return crypt.crypt(password, salt)
+
 
 # User registration Function.  Checks password for complexity and for
 # matching.  If it passes the checks it then hashes the password and stores
@@ -91,7 +147,7 @@ def hash_password(password, salt):
 # TODO: Needs cleanup. The code is messy right now and has some redundancies.
 def user_registration(file):
     name = input("Enter Full name: ")
-    email = register_email() #get the user's email
+    email = register_email()  # get the user's email
     password = get_password()
     second_pass = getpass("Re-enter Password: ")
     check = password_check(password)
@@ -101,14 +157,19 @@ def user_registration(file):
         if password != second_pass:
             print("Passwords don't match.")
         elif not check.get("password_ok"):
-            print(
-                "Password not strong enough. A password is considered strong if:"
-                "\n8 characters length or more"
-                "\n1 digit or more"
-                "\n1 symbol or more"
-                "\n1 uppercase letter or more"
-                "\n1 lowercase letter or more."
-            )
+            print("Password not strong enough:")
+            # conditions to print the appropriate password error
+            if check.get("length_error"):
+                print("Password must have 8 character or more ")
+            if check.get("digit_error"):
+                print("Password must have at least 1 digit")
+            if check.get("uppercase_error"):
+                print("Password must have at least 1 uppercase letter")
+            if check.get("lowercase_error"):
+                print("Password must have at least 1 lowercase letter")
+            if check.get("symbol_error"):
+                print("Passowrd must have at least 1 special character")
+        
         password = getpass("Enter password:")
         second_pass = getpass("Re-enter Password")
         check = password_check(password)
@@ -119,30 +180,30 @@ def user_registration(file):
     dict = {}
     dict["Users"] = []
     dict["Users"].append({email: {"name": name, "password": h_password, "salt": salt}})
-    json.dump(dict, f)
+    json.dump(dict, file)
     file.close()
-    #require user to re-login once they have registered (for security reasons)
+    # require user to re-login once they have registered (for security reasons)
     print("User sucessfully registered, please login again to begin...")
     exit()
 
-# TODO: Implement code to check if email is registered
+
 def login_helper():
-    #function login_helper allows the user to try to enter 
-    #their password a maximum of three times before exiting
+    # function login_helper allows the user to try to enter
+    # their password a maximum of three times before exiting
     number_of_password_login_attempts = 0
     bool = False
     email = check_email()
-    #if email is None then the user mis-entered their email three times, so exit
-    
+    # if email is None then the user mis-entered their email three times, so exit
+
     json_data = open(USER_LIST)
     jdata = json.load(json_data)
-    user_data = jdata['Users']
-    
+    user_data = jdata["Users"]
+
     # case where email exists in JSON file
     if email in user_data[0]:
-        print('Email exists, you may proceed!')
+        print("Email exists, you may proceed!")
         while number_of_password_login_attempts < MAX_TIMES_ALLOWED_TO_ENTER:
-            password = get_password() #get the user's password attempt
+            password = get_password()  # get the user's password attempt
             old_pass = ""
             salt = ""
             with open(USER_LIST) as f:
@@ -158,74 +219,119 @@ def login_helper():
             else:
                 print("password is incorrect")
                 bool = False
-                number_of_password_login_attempts +=1
-    
+                number_of_password_login_attempts += 1
+
     elif email not in user_data[0]:
-        print('\nUser with that email not registered!')
-        print('Exiting\n')
-        
+        print("\nUser with that email not registered!")
+        print("Exiting\n")
+
     return bool
 
 
 # TODO: Implement session ID to keep login relevant
 def user_login():
-    #if login_helper fails (returns False), then exit the program as 
-    #the user mis-entered their email or password more than 3 times
+    # if login_helper fails (returns False), then exit the program as
+    # the user mis-entered their email or password more than 3 times
     if not login_helper():
         exit()
 
-#Function to see if "contacts" field exists in json file
+
+# Function to see if "contacts" field exists in json file
 def contacts_exist(user_dictionary):
-    if 'contacts' in user_dictionary:
+    if "contacts" in user_dictionary:
         return True
     return False
 
-#Function to get contact info from user
-def add_contact():
+
+# Function to get contact info from user
+def contact_details():
     name = input("Enter Full Name: ")
     email = input("Enter Email Address: ")
     return (email, name)
 
-#Function to check if the email the user wants to register as a contact
-#already exists 
+
+# Function to check if the email the user wants to register as a contact
+# already exists
 def user_contact_exist(user_dictionary, email):
     count = 0
     for x in user_dictionary:
-        if user_dictionary[count].get('email') == email:
+        if user_dictionary[count].get("email") == email:
             return True
         count += 1
     return False
 
-#function that removes old email ID and builds new email ID
+
+# function that removes old email ID and builds new email ID
 # TODO: Clean up code -> need to add a boolean function that just checks if
 #                        contacts exists (will move rest of code to do_add function)
 def update_user_contact(email, name):
     count = 0
     fp = open("user_list.json", "r+")
-    data = json.load(fp) #load all json data into a string
-        
-    #get the user email
-    user_email = list(data['Users'][0].keys())[0]
+    data = json.load(fp)  # load all json data into a string
+
+    # get the user email
+    user_email = list(data["Users"][0].keys())[0]
     print(user_email)
-    #get all data associated with user
-    u_dictionary = data['Users'][0][user_email]['contacts']
+    # get all data associated with user
+    u_dictionary = data["Users"][0][user_email]["contacts"]
     print("initial u_dictionary: ", u_dictionary)
     for x in u_dictionary:
-        if u_dictionary[count].get('email') == email:
+        if u_dictionary[count].get("email") == email:
             u_dictionary.pop(count)
-            u_dictionary.append({'email': email, 'name': name})
+            u_dictionary.append({"email": email, "name": name})
             print("updated u_dictionary after pop/append: ", u_dictionary)
-            #print("data before the clear: ", data)
-            #data['Users'][0][user_email]['contacts'].clear()
-            #print("data after the clear: ", data)
-            #data['Users'][0][user_email]['contacts'].append(u_dictionary)
-            #print("data after addition of u_dictionary ", data)
             fp.seek(0)
             json.dump(data, fp)
-            fp.close()    
+            fp.close()
             return True
         count += 1
-    return False 
+    return False
+
+
+def add_contact():
+    fp = open("user_list.json", "r+")
+    data = json.load(fp)  # load all json data into a string
+
+    # get the user email
+    user_email = list(data["Users"][0].keys())[0]
+
+    # get all data associated with user
+    u_dictionary = data["Users"][0][user_email]
+
+    if contacts_exist(u_dictionary):  # if user has already added a contact
+        email, name = contact_details()
+        email_exist = user_contact_exist(
+            data["Users"][0][user_email]["contacts"], email
+        )  # check to see if the email the user wants to add already exists
+        if email_exist:  # email already exists
+            # data['Users'][0][user_email]['contacts'].clear()
+            # data['Users'][0][user_email]['contacts'] = u_d_temp
+            # fp.seek(0)
+            # json.dump(data, fp)
+            fp.close()
+            update_user_contact(email, name)
+            # print("Contact Added.")
+        else:  # email does not exist
+            data["Users"][0][user_email]["contacts"].append(
+                {"email": email, "name": name}
+            )
+            fp.seek(0)
+            json.dump(data, fp)
+            fp.close()
+        print("Contact Added.")
+    else:  # user has never added a contact before
+        email, name = contact_details()  # get email and name the user wants to add
+        u_dictionary["contacts"] = [
+            {"email": email, "name": name}
+        ]  # create new field name -> contacts
+        data["Users"][0][
+            user_email
+        ] = u_dictionary  # add the email and name the user wants to contact to their contacts
+        fp.seek(0)  # return file pointer to beginning of json file
+        json.dump(data, fp)  # add new information to json file
+        print("Contact Added.")
+        fp.close()
+
 
 class MyPrompt(Cmd):
     prompt = "secure_drop> "
@@ -238,46 +344,13 @@ class MyPrompt(Cmd):
         print("exit the application. Shorthand: x q Ctrl-d")
 
     def do_add(self, inp):
-        fp = open("user_list.json", "r+")
-        data = json.load(fp) #load all json data into a string
-    
-        #get the user email
-        user_email = list(data['Users'][0].keys())[0]
-        
-        #get all data associated with user
-        u_dictionary = data['Users'][0][user_email]
-
-        if contacts_exist(u_dictionary): #if user has already added a contact
-            email, name = add_contact()
-            email_exist = user_contact_exist(data['Users'][0][user_email]['contacts'], email) #check to see if the email the user wants to add already exists
-            if email_exist: #email already exists
-                #data['Users'][0][user_email]['contacts'].clear()
-                #data['Users'][0][user_email]['contacts'] = u_d_temp
-                #fp.seek(0)
-                #json.dump(data, fp)
-                fp.close()
-                update_user_contact(email, name)
-                #print("Contact Added.")
-            else: #email does not exist
-                data['Users'][0][user_email]['contacts'].append({'email': email, 'name': name})
-                fp.seek(0)
-                json.dump(data, fp)
-                fp.close()
-            print("Contact Added.")
-        else: #user has never added a contact before
-            email, name = add_contact() #get email and name the user wants to add
-            u_dictionary['contacts'] = [{'email': email, 'name': name}] #create new field name -> contacts
-            data['Users'][0][user_email] = u_dictionary #add the email and name the user wants to contact to their contacts 
-            fp.seek(0) #return file pointer to beginning of json file 
-            json.dump(data, fp) #add new information to json file
-            print("Contact Added.")
-            fp.close()
+        add_contact()
 
     def help_add(self):
         print("Add a new contact.")
 
     def do_list(self, inp):
-        print("list contacts")
+        print(ONLINE_CONTACTS)
 
     def help_list(self):
         print("List all online contacts.")
@@ -290,14 +363,19 @@ class MyPrompt(Cmd):
     do_EOF = do_exit
     help_EOF = help_exit
 
-if __name__ == "__main__":
 
+def start_cmd():
+    MyPrompt().cmdloop()
+
+
+# Handles the initial login. If there is not registered users, the function
+# will run user_registration(file)
+def start_up():
     """
     The user registration is a one-time process. Once a user is registered on a
     client, the login module is activated subsequently. After a successful
     login, a "secure_drop>" shell is started.
     """
-    # could potentially use open("user_list, x") in a try catch for this
     if os.path.isfile(USER_LIST) and os.path.getsize(USER_LIST) > 0:
         user_login()
     else:
@@ -314,4 +392,20 @@ if __name__ == "__main__":
         elif inp == "n":
             exit(1)
 
-    MyPrompt().cmdloop() 
+
+def run():
+    start_up()
+
+    sc = ScanTask()
+    scanner = Thread(target=sc.scan_for_online_contacts)
+    listener = Thread(target=listen_for_scan)
+    cmd = Thread(target=start_cmd)
+
+    scanner.start()
+    listener.start()
+    cmd.start()
+    cmd.join()
+    listener.join()
+
+    sc.terminate()
+    scanner.join()
