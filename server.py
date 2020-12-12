d5ed2dee6fd6733eb80fddfb358b7ec4e1f557d7
Yorgo Kennos <yorgokennos@gmail.com>
2020-12-12 15:24:12 -0500

    Update server.py
    using the previous version, which does not include a loading bar for simplicity sake.

--- server.py
+++ server.py
@@ -1,56 +1,35 @@
+#server.py: i incorrectly renamed this file as client.py
 import socket
-import tqdm
-import os
+import sys 
 
-# all IPv4 address on the local machine
-# if the server has two IPs on a network it should be reachable
-# by both
-SERVER_HOST = "0.0.0.0"
-SERVER_PORT = 9999
-#receive 4096 bytes each time 
-BUFFER_SIZE = 4096
-SEPARATOR = "<SEPARATOR>"
-
-#TCP socket 
 s = socket.socket()
-s.bind((SERVER_HOST, SERVER_PORT))
+s.bind(('localHost', 9999))
+s.listen(10)
+sc, address = s.accept()
 
-# 5 is the number of unaccepted connections that the
-# system will allow before refusal
-s.listen(5)
-print(f"\tListening as {SERVER_HOST}:{SERVER_PORT}")
+print(f"address: {address}")
+print(f"     SC: {sc}")
 
-#accept connection if there is any
-client_socket, address = s.accept()
-# code below this line will only execute if a connection 
-# has been established
-print(f"\t{address} is connected")
+# print(address)
 
-received = client_socket.recv(BUFFER_SIZE).decode()
-file_name, file_size = received.split(SEPARATOR)
 
-# remove absolute file path if included
-file_name = os.path.basename(file_name)
 
-#convert our file size to an int
-file_size = int(file_size)
+# open in binary
 
-#start receiving file from our socket
-# THIS COULD VERY WELL BE A FUNCTION ON ITS OWN
-progress = tqdm.tqdm(range(file_size), f"Receiving {file_name}", unit = "B", unit_scale = True, unit_divisor = 1024)
-with open(file_name, "wb") as f:
-    for _ in progress:
-        # read 1024 bytes from the socket
-        bytes_read = client_socket.recv(BUFFER_SIZE)
-        if not bytes_read:
-            # nothing received 
-            break
-        # write to file with our received bytes
-        f.write(bytes_read)
-        # update fancy progress bar
-        progress.update(len(bytes_read))
+f = open('receivedFile.pdf', 'wb')
 
-#close our client socket
-client_socket.close()
-#close our server socket
-s.close()
+
+print('For debugging, printing raw bytes of file...')
+
+while True:
+    l = sc.recv(1026)
+    while l:
+        f.write(l)
+        l = sc.recv(1026)
+        print(l)
+
+    #f.close()
+    #sc.close()
+
+# s.close()
+
