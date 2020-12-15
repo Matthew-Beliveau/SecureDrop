e2a9ebf5f508f754303bbd80db392ce5d69896a8
Yorgo Kennos <yorgokennos@gmail.com>
2020-12-14 23:21:37 -0500

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,12 +1,17 @@
 import re
 from getpass import getpass
 import crypt
+import json
 
 MAX_TIMES_ALLOWED_TO_ENTER = 3
 
 USER_LIST = "./user_list.json"
 
 ONLINE_CONTACTS = []
+
+USER_EMAIL = ""
+
+USER_NAME = ""
 
 
 def password_check(password):
@@ -93,3 +98,51 @@ def check_email():
 
 def hash_password(password, salt):
     return crypt.crypt(password, salt)
+
+
+# Function to check if the email the user wants to register as a contact
+# already exists
+def user_contact_exist(user_dictionary, email):
+    count = 0
+    for x in user_dictionary:
+        if user_dictionary[count].get("email") == email:
+            return True
+        count += 1
+    return False
+
+
+def get_user_name_from_list():
+    fp = open("user_list.json", "r+")
+    data = json.load(fp)  # load all json data into a string
+    fp.close()
+
+    user_email = list(data["Users"][0].keys())[0]
+    return data["Users"][0][user_email]["name"]
+
+
+def check_user_contact(data):
+    fp = open("user_list.json", "r+")
+    file_data = json.load(fp)
+
+    user_email = list(file_data["Users"][0].keys())[0]
+
+    email_exists = user_contact_exist(
+        file_data["Users"][0][user_email]["contacts"], data[1]
+    )
+    fp.close()
+    return email_exists
+
+
+# Function to see if "contacts" field exists in json file
+def contacts_dict_exist():
+    fp = open("user_list.json", "r+")
+    data = json.load(fp)  # load all json data into a string
+
+    # get the user email
+    user_email = list(data["Users"][0].keys())[0]
+
+    # get all data associated with user
+    u_dictionary = data["Users"][0][user_email]
+    if "contacts" in u_dictionary:
+        return True
+    return False
